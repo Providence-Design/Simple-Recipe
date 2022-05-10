f15f1dc8b888e1a50935e1bcd58d55f8a6f16350
Providence-Design <baabaprovidence@gmail.com>
2022-05-10 17:19:58 +0000

    another commit

--- src/main/java/SimpleRecommendationSystem/recommendation/model/Recipe.java
+++ src/main/java/SimpleRecommendationSystem/recommendation/model/Recipe.java
@@ -1,11 +1,22 @@
 package SimpleRecommendationSystem.recommendation.model;
 
 import javax.persistence.Id;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Recipe {
     @Id
+
     private Long id;
     private String cuisine;
+    private List<String> ingredients = new ArrayList<String>();
+    private List<String> instructions = new ArrayList<String>();
 
 
+    public Recipe(Long id, String cuisine, List<String> ingredients, List<String> instructions) {
+        this.id = id;
+        this.cuisine = cuisine;
+        this.ingredients = ingredients;
+        this.instructions = instructions;
+    }
 }
